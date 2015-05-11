b95a0f9427850ec8b5f383be0cb4150cabf73295
sileste <sileste@gmx.com>
2015-05-11 02:28:43 +0200

    basic
    greek alphabet

---
+++ greek.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset=utf-8>
+    <title>aleph document</title>
+ 
+    <link rel="stylesheet" type="text/css" href="fst.css">
+
+  </head>
+  <body>
+<article>
+<header>
+    <h1>Hello world !</h1>
+    <time pubdate datetime="2013-12-18"></time>
+</header>
+<?php
+
+echo "Aktualny cas: ".date("G:i")."
+";
+
+
+echo "lastmod: ".strftime("%m/%d/%Y %H:%M:%S",getlastmod());
+
+
+?>
+
+<b>Greek Alphabet</b>
+#1: &alpha; - alpha
+#2:  &beta; - beta
+#3:  &gamma; - gamma
+#4:  &delta; - delta
+#5:  &epsilon; - epsilon
+#6:  &zeta; - zeta
+#7:  &eta; - eta
+#8:  &theta; - theta
+#9:  &iota; - iota
+#10: &kappa; - kappa
+#11: &lambda; - lambda
+#12: &mu; - mu
+#13: &nu; - nu
+#14: &xi; - xi
+#15: &omicron; - omicron
+#16: &pi; - pi
+#17: &rho; - rho
+#18: &sigma; - sigma
+#19: &tau; - tau
+#20: &upsilon; - upsilon
+#21: &phi; - phi
+#22: &chi; - chi
+#23: &psi; - psi
+#24: &omega; - omega
+    <footer>#1</footer> 
+<form>
+Vlakno: <input type="text"><br> 
+<input type="submit" value="Gen">
+</form>
+
+
+    <script language="javascript" type="text/javascript">
+<!--
+document.write('JavaScript!');
+-->
+
+    </script>
+<footer> 
+ * In Memoriam Leonhard Paul Euler<br>
+</footer>
+</article>
+
+  </body>	
+</html>
